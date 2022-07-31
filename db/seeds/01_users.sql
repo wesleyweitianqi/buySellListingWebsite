5c6f295cf7d71912f7d134d814ff89f3a6263bfa
SFxImpulse <braverman.david.business@gmail.com>
2022-07-31 15:58:25 +0000

    Add seeds for users and favourite_items tables.

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,11 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password)
+VALUES ('David', 'david@braverman.com', '123'),
+('Rae', 'rae@rain.com', '456'),
+('Timone', 'timone@rain.com', '789'),
+('Wesley', 'wesley@wei.com', '134'),
+('Mingu', 'mingu@kim.com', '567'),
+('Alice', 'alice@gurkin.com', '890'),
+('Jared', 'jared@gorp.com', '145'),
+('Claire', 'claire@redfield.com', '678'),
+('Ethan', 'ethan@winters.com', '901'),
+('Abigail', 'abigail@wizard.com', '156');
