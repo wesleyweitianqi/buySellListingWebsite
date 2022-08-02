b1c2e4ba48d1861a66870d680f5e81bc24534e7c
SFxImpulse <braverman.david.business@gmail.com>
2022-08-02 17:34:29 +0000

    linting

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get("/", (req, res) => {
       return res.redirect('/');
     }).catch(err => console.error(err));
   } else {
-  res.render("index", {user_id: ''});
+    res.render("index", {user_id: ''});
   }
 });
 
