376a9823db9811aacdd2ef8217a46302e4b9bb51
Wesley Wei <wesleytianqiwei@gmail.com>
2022-08-02 17:34:13 -0400

    clear code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,37 +42,5 @@ $(document).ready(function() {
     }
   })
 });
-//   // $.ajax({
-//   //   url: '/user/post',
-//   //   method: 'GET',
-//   //   success: function() {
-//   //     $('.form').append(postform);
-//   //   }
-//   // })
-//   const $postform = $('#form');
-//   console.log($postform);
-//   $postform.on('submit', function(event) {
-//     event.preventDefault();
-//     const input = $(this).serializeArray();
-//     console.log(input);
-//     $.ajax({
-//       url: '/user/post',
-//       method:'POST',
-//       data: input,
-//       success: function() {
-
-//         $.ajax({
-//           url: '/user/post',
-//           method:'GET',
-//           data:'json',
-//           success: function(data) {
-//             console.log('post:',data);
-//             $('.post-container').append(data);
-//           }
-//         })
-//       }
-//     })
-//   })
-// });
 
 
