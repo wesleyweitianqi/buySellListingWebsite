1553d849f823e6bf91f9fbbf91470daed9c7442a
Wesley Wei <wesleytianqiwei@gmail.com>
2022-08-03 14:54:26 -0400

    post items, display post, show all lists, show specific user list, refactor routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,11 +9,11 @@ const escape = function (str) {
 const createListing = function(listingObj) {
   const $listing = `
   <article class="listing">
-    <img class="listing_photo" src="${listingObj.photo_url}>
+    <img class="listing_photo" src="${listingObj.photo_url}">
     <section class="description">
       <div>
-        <span class="listing_text">${listingObj.brand}</span>
-        <span class="listing_text">${listingObj.model}</span>
+        <span class="listing_text">${escape(listingObj.brand)}</span>
+        <span class="listing_text">${escape(listingObj.model)}</span>
         <span class="listing_text">$${listingObj.price / 100}</span>
         <span class="listing_text">${listingObj.is_sold}</span>
       <div>
@@ -29,37 +29,60 @@ const appendListing = function(listingArray) {
   }
 };
 
-const postlisting = function(listingArray) {
+const postListing = function(listingArray) {
   for (let listing of listingArray) {
-    $('.post-container').append(createListing(listing));
+    $('.post-container').prepend(createListing(listing));
+  }
+};
+
+const othersListing = function(listingArray) {
+  for (let listing of listingArray) {
+    $('.others_container').prepend(createListing(listing));
   }
 };
 
 $(document).ready(function() {
   $.ajax({
-    url:'/listings/api',
+    url:'/api/listings',
     method:'GET',
     success: function(data) {
       appendListing(data);
     }
   });
- const postListing = $('.post-container');
- const inputArray = postListing.serializeArray();
- postListing.submit((event) => {
+
+  $.ajax({
+    url:'/api/listings/me',
+    method:'GET',
+    success: function(data) {
+      postListing(data);
+    }
+  });
+
+
+  $.ajax({
+    url:`/api/listings/others/${$('#other_user_id').text()}`,
+    method:'GET',
+    success: function(data) {
+      othersListing(data);
+    }
+  });
+
+
+ $('#form').submit((event) => {
   event.preventDefault();
+  const formdata = {};
+  $("#form").serializeArray().map(function(x){formdata[x.name] = x.value;});
+  console.log(formdata);
     $.ajax({
-      url: '/:user_id',
-      method: 'post',
-      data: inputArray,
-      success: function(data) {
-        console.log(data);
-        $.get('/:user_id', function() {
-          const $new = $('<h1>ok</h>')
-          $('.post-container').append($new);
+      url: '/api/listings',
+      method: 'POST',
+      data:formdata,
+      success: function() {
+        $.get('/api/listings/me', function(data) {
+          $('.post-container').prepend(createListing(data[0]));
         })
       }
     });
   });
 });
-
 
--- routes/listings.js
+++ routes/listings.js
@@ -2,17 +2,60 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM listings;`)
-      .then(data => {
-        const listings = data.rows;
-        res.json({ listings });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  //create - post
+  router.post('/',(req,res) => {
+    console.log(req.body)
+    // db.query('SELECT * FROM users where id = $1;', [req.session.user_id]).then(result => {
+      // res.render('post', {user_id: req.session.user_id, id: result.rows[0].name, username: result.rows[0].name });
+      const text = "INSERT INTO listings (user_id, brand, model, year, description, price, is_sold, photo_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *";
+      const params = [req.session.user_id, req.body.brand, req.body.model, req.body.year, req.body.description, req.body.price, false, req.body.photo_url];
+      console.log(params);
+      db.query(text, params).then(() => {
+        res.redirect('/listings/new')
+      }).catch(err =>console.error(err));
+    // }).catch(err => console.error(err));
+
   });
+
+  //read all - get
+  router.get('/', (req, res) => {
+    if (req.session.user_id) {
+      db.query('SELECT * FROM users WHERE id = $1;', [req.session.user_id]).then(result => {
+        const templateVars = {user_id: req.session.user_id, id: result.rows[0].name, username: result.rows[0].name }
+        db.query('SELECT * FROM listings ORDER BY time_created DESC LIMIT 10;').then(result => {
+          const listings = result.rows;
+          res.send(listings);
+        }).catch(err => console.error(err));
+      }).catch(err => console.error(err));
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+  //read all from user - get
+  router.get('/me', (req,res) => {
+    if (req.session.user_id) {
+      db.query('SELECT * FROM users WHERE id = $1;', [req.session.user_id]).then(result => {
+        const templateVars = {user_id: req.session.user_id, id: result.rows[0].name, username: result.rows[0].name }
+        db.query('SELECT * FROM listings WHERE user_id =$1 ORDER BY time_created DESC LIMIT 10;',[req.session.user_id]).then(result => {
+          const listings = result.rows;
+          res.send(listings);
+        }).catch(err => console.error(err));
+      }).catch(err => console.error(err));
+    }
+  });
+
+    //read all from another specific user - get
+    router.get('/others/:user_id', (req,res) => {
+      if (req.session.user_id) {
+        db.query('SELECT * FROM users WHERE id = $1;', [req.session.user_id]).then(result => {
+          const templateVars = {user_id: req.session.user_id, id: result.rows[0].name, username: result.rows[0].name }
+          db.query('SELECT * FROM listings WHERE user_id = $1 ORDER BY time_created DESC LIMIT 10;',[req.params.user_id]).then(result => {
+            const listings = result.rows;
+            res.send(listings);
+          }).catch(err => console.error(err));
+        }).catch(err => console.error(err));
+      }
+    });
   return router;
 };
