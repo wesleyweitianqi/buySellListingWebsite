32358d173626b2c170ad74afe5e923c6496a0762
SFxImpulse <braverman.david.business@gmail.com>
2022-08-04 20:33:02 +0000

    starting email.

--- routes/frontend.js
+++ routes/frontend.js
@@ -37,7 +37,8 @@ module.exports = (db) => {
     router.get('/listings', (req, res) => {
       if (req.session.user_id) {
         db.query('SELECT * FROM users WHERE id = $1;', [req.session.user_id]).then(result => {
-          const templateVars = {user_id: req.session.user_id, id: result.rows[0].name, username: result.rows[0].name }
+          const templateVars = {user_id: req.session.user_id, id: result.rows[0].name, username: result.rows[0].name, email: result.rows[0].email };
+          console.log(templateVars.email);
           res.render('listings', templateVars);
         }).catch(err => console.error(err));
       } else {
