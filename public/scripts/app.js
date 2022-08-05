59dd16741b84c875927443ea05da8ee79bc035ad
Mingu Kim <mkim245@gmail.com>
2022-08-04 21:19:17 -0400

    update navbar listing mainpage style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,69 +6,71 @@ const escape = function (str) {
   return div.innerHTML;
 };
 
-const createListing = function(listingObj) {
+const createListing = function (listingObj) {
   const $listing = `
   <article class="listing">
     <img class="listing_photo" src="${listingObj.photo_url}">
-    <section>
-      <div class="description">
-      <span class="listing_text">${escape(listingObj.brand.toUpperCase())}</span>
-        <span class="listing_text">${escape(listingObj.model)}</span>
-        <span class="listing_text">${escape(listingObj.description)}</span>
-        <div class="listing_text">$${listingObj.price.toLocaleString('en-US')}</div>
-        <span class= ${listingObj.is_sold ? "listing_text_sale": "listing_text"}>${listingObj.is_sold ? "on sale":"sold"}</span>
+    <section class= "list_details">
+      <div class="brand_model">
+        <span class="brand">${escape(listingObj.brand.toUpperCase())}</span>
+        <span class="model">${escape(listingObj.model)}</span>
+      </div> 
+        <span class="description">${escape(listingObj.description)}</span>
+      <div class="price_sold">
+        <div class="price">$${listingObj.price.toLocaleString('en-US')}</div>
+        <span class= ${listingObj.is_sold === "True" ? "listing_text" : "listing_text_sale"}>${listingObj.is_sold === "True" ? "sold" : "on sale"}</span>
         <button method="POST" action="/delete" type="button" class="btn btn-info"><i class="fa-regular fa-heart"></i></button>
-      <div>
+      </div>
     </section>
   </article>
   `;
   return $listing;
 };
 
-const appendListing = function(listingArray) {
+const appendListing = function (listingArray) {
   for (let listing of listingArray) {
     $('.listing_container').append(createListing(listing));
   }
 };
 
-const postListing = function(listingArray) {
+const postListing = function (listingArray) {
   for (let listing of listingArray) {
     $('.post-container').append(createListing(listing));
   }
 };
 
-const othersListing = function(listingArray) {
+const othersListing = function (listingArray) {
   for (let listing of listingArray) {
     $('.others_container').append(createListing(listing));
   }
 };
 
-const searchListing = function(listingArray) {
+const searchListing = function (listingArray) {
   for (let listing of listingArray) {
     $('.search_container').append(createListing(listing));
   }
 };
 
-const favouriteListing = function(listingArray) {
+const favouriteListing = function (listingArray) {
   for (let listing of listingArray) {
     $('.favourite_container').prepend(createListing(listing));
   }
 };
 
 
-$(document).ready(function() {
+$(document).ready(function () {
   $.ajax({
-    url:'/api/listings',
-    method:'GET',
-    success: function(data) {
+    url: '/api/listings',
+    method: 'GET',
+    success: function (data) {
       appendListing(data);
     }
   });
 
   $.ajax({
-    url:'/api/listings/me',
-    method:'GET',
-    success: function(data) {
+    url: '/api/listings/me',
+    method: 'GET',
+    success: function (data) {
       postListing(data);
     }
   });
@@ -76,33 +78,17 @@ $(document).ready(function() {
   $.ajax({
     url: '/api/listings/favourite',
     method: 'GET',
-    success: function(data) {
+    success: function (data) {
       favouriteListing(data);
     }
   })
 
   $.ajax({
     url: '/search/api',
-    method:'GET',
-    success: function(data) {
+    method: 'GET',
+    success: function (data) {
       $('.search_container').val('');
       searchListing(data);
     }
   })
-// })
-  // const $searchform = $('.search_form');
-  // $searchform.submit(function(event) {
-  //   event.preventDefault();
-  //   const input = $(this).serializeArray()//.map(function(x){formdata[x.name] = x.value;});
-  //   // console.log('input:',input);
-  //   console.log('----------------------------------------')
-  // $.ajax({
-  //   url: '/api/listings/search',
-  //   method: 'GET',
-  //   success: function(data) {
-  //     $('.search_container').val('');
-  //     searchListing(data);
-  //   }
-  // });
-  // });
 });
